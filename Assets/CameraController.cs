02c05ced0ef3e67d719138063366b7b7fc493a22
BuildTools <unconfigured@null.spigotmc.org>
2019-12-31 23:14:24 -0500

    camera orbit script complete

---
+++ Assets/CameraController.cs
@@ -0,0 +1,158 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    Vector3 mouse_position;
+    Vector3 old_position;
+    Vector3 rotate_amount_x;
+    Vector3 rotate_amount_y;
+    float width = (float) Screen.width;
+    float height = (float) Screen.height;
+    float fracx;
+    float fracy;
+    float zoom_amount;
+    float zoom_frac;
+    Transform pitch;
+    Transform camera;
+
+    public int sensitivity = 200;
+    public float zoom_sensitivity = .5f;
+
+    bool down = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pitch = gameObject.transform.GetChild(0);       
+        camera = pitch.GetChild(0);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log("Pressed secondary button.");
+            mouse_position.x = Input.mousePosition.x / width;
+            mouse_position.y = Input.mousePosition.y / height;
+            old_position = mouse_position;
+            down = true;
+        }
+        if (Input.GetMouseButtonUp(1))
+        {
+            Debug.Log("Released secondary button.");
+            down = false;
+        }
+        if (down)
+        {
+            // The mouse is down, we want to be moving the camera if old is different from
+            // current
+            mouse_position.x = Input.mousePosition.x / width;
+            mouse_position.y = Input.mousePosition.y / height;
+            if (mouse_position != old_position)
+            {
+                rotate_amount_x = new Vector3(0f, (mouse_position.x - old_position.x) * sensitivity, 0f);
+                gameObject.transform.Rotate(rotate_amount_x, Space.World);
+                rotate_amount_y = new Vector3((old_position.y - mouse_position.y) * sensitivity, 0f, 0f); 
+
+                if (pitch.eulerAngles.x + rotate_amount_y.x > 44 &&
+                    pitch.eulerAngles.x + rotate_amount_y.x < 180)
+                {
+                    pitch.Rotate(new Vector3(45f - pitch.eulerAngles.x, 0f, 0f));
+                }
+                else if (pitch.eulerAngles.x + rotate_amount_y.x < 316 &&
+                         pitch.eulerAngles.x + rotate_amount_y.x > 180)
+
+                {
+                    pitch.Rotate(new Vector3(-45f - pitch.eulerAngles.x, 0f, 0f));
+                }
+                else
+                    pitch.Rotate(rotate_amount_y);
+            }
+            old_position = mouse_position;
+            fracx = Mathf.Abs(rotate_amount_x.y / 20f);
+            fracy = Mathf.Abs(rotate_amount_y.x / 20f);
+        }
+        else
+        {
+            if (rotate_amount_x.y != 0)
+            {
+                // The mouse is not down, we want to gradually slow down the previous rotation
+                if (rotate_amount_x.y < fracx && rotate_amount_x.y > - fracx)
+                    rotate_amount_x.y = 0;
+                else if (rotate_amount_x.y < 0)
+                    rotate_amount_x.y = rotate_amount_x.y + fracx;
+                else
+                    rotate_amount_x.y = rotate_amount_x.y - fracx;
+
+                gameObject.transform.Rotate(rotate_amount_x);
+            }
+
+            if (rotate_amount_y.x != 0)
+            {
+                if (rotate_amount_y.x < fracy && rotate_amount_y.x > -fracy)
+                    rotate_amount_y.x = 0;
+                else if (rotate_amount_y.x < 0)
+                {
+                    rotate_amount_y.x = rotate_amount_y.x + fracy;
+                }
+                else
+                {
+                    rotate_amount_y.x = rotate_amount_y.x - fracy;
+                }
+
+                Debug.Log(rotate_amount_y.x);
+
+
+
+                if (pitch.eulerAngles.x + rotate_amount_y.x > 45 &&
+                    pitch.eulerAngles.x + rotate_amount_y.x < 180)
+
+                {
+                    pitch.Rotate(new Vector3(45f - pitch.eulerAngles.x, 0f, 0f));
+                }
+                else if (pitch.eulerAngles.x + rotate_amount_y.x < 315 &&
+                         pitch.eulerAngles.x + rotate_amount_y.x > 180)
+
+                {
+                    pitch.Rotate(new Vector3(-45f - pitch.eulerAngles.x, 0f, 0f));
+                }
+                else
+                    pitch.Rotate(rotate_amount_y);
+            }
+        }
+
+        // We also check the scroll wheel delta and zoom accordingly from 3 to 30
+        if (Input.mouseScrollDelta.y != 0)
+        {
+            float input = -Input.mouseScrollDelta.y;
+            if (input < 0)
+                zoom_amount = (input * zoom_sensitivity * (camera.localPosition.z + (input * zoom_sensitivity * camera.localPosition.z)));
+            else
+                zoom_amount = (input * zoom_sensitivity * camera.localPosition.z);
+            zoom_frac = Mathf.Abs(zoom_amount / 5);
+        }
+
+        Debug.Log("ZOOM: " + zoom_amount);
+
+        if (zoom_amount != 0)
+        {
+            if (zoom_amount < zoom_frac &&  zoom_amount > - zoom_frac)
+                zoom_amount = 0;
+            else if (zoom_amount < 0)
+                zoom_amount += zoom_frac;
+            else
+                zoom_amount -= zoom_frac;
+
+            if (camera.localPosition.z + zoom_amount < -30)
+                camera.Translate(new Vector3(0f, 0f, -30 - camera.localPosition.z));
+            else if (camera.localPosition.z + zoom_amount > -3)
+                camera.Translate(new Vector3(0f, 0f, -3 - camera.localPosition.z));
+            else
+                camera.Translate(new Vector3(0f, 0f, zoom_amount));
+        }
+    }
+}
--- Assets/MainMenuController.cs
+++ Assets/MainMenuController.cs
@@ -12,8 +12,53 @@ public class MainMenuController : MonoBehaviour
     public Text[] player_labels;
 
     string name;
+    int players_in_lobby;
 
-    // Start is called before the first frame update
+    protected Callback<LobbyCreated_t> Callback_lobbyCreated;
+    protected Callback<LobbyMatchList_t> Callback_lobbyList;
+    protected Callback<LobbyEnter_t> Callback_lobbyEnter;
+    protected Callback<LobbyDataUpdate_t> Callback_lobbyInfo;
+    protected Callback<GameLobbyJoinRequested_t> Callback_joinRequested;
+    protected Callback<LobbyChatUpdate_t> Callback_chatUpdate;
+
+    ulong current_lobbyID;
+    Steamworks.CSteamID lobbyID;
+    List<CSteamID> lobbyIDS;
+
+    // Use this for initialization
+    void Start () {
+
+        lobby_canvas.enabled = false;
+
+        lobbyIDS = new List<CSteamID>();
+
+        if (SteamAPI.Init())
+        {
+            Debug.Log("Steam API init -- SUCCESS!");
+            name = SteamFriends.GetPersonaName();
+            //player_labels[0].text = name;
+        }
+        else
+        {
+            Debug.Log("Steam API init -- failure ...");
+        }
+    }
+
+    private void OnEnable()
+    {
+        if (SteamManager.Initialized) {
+            Debug.Log("Enabled!");
+
+            Callback_lobbyCreated = Callback<LobbyCreated_t>.Create(OnLobbyCreated);
+            Callback_lobbyList = Callback<LobbyMatchList_t>.Create(OnGetLobbiesList);
+            Callback_lobbyEnter = Callback<LobbyEnter_t>.Create(OnLobbyEntered);
+            Callback_lobbyInfo = Callback<LobbyDataUpdate_t>.Create(OnGetLobbyInfo);
+            Callback_joinRequested = Callback<GameLobbyJoinRequested_t>.Create(OnJoinRequested);
+            Callback_chatUpdate = Callback<LobbyChatUpdate_t>.Create(OnChatUpdate);
+        }
+    }
+
+    /*/ Start is called before the first frame update
     void Start()
     {
         // We start by hiding the lobby canvas, this will be shown later
@@ -23,21 +68,104 @@ public class MainMenuController : MonoBehaviour
         {
             name = SteamFriends.GetPersonaName();
             player_labels[0].text = name;
-            SteamMatchmaking.CreateLobby(ELobbyType.k_ELobbyTypeFriendsOnly, 4);
+            LobbyCreated_t lobby_status = (LobbyCreated_t) SteamMatchmaking.CreateLobby(ELobbyType.k_ELobbyTypeFriendsOnly, 4);
         }
         
-    }
+    }*/
 
     public void OnPlayButtonClick()
     {
-        lobby_canvas.enabled = true;
-        main_menu_canvas.enabled = false;
+        SteamAPICall_t lobby_status = SteamMatchmaking.CreateLobby(ELobbyType.k_ELobbyTypeFriendsOnly, 4);
     }
 
     public void OnBackButtonClick()
     {
         lobby_canvas.enabled = false;
         main_menu_canvas.enabled = true;
+    }
+
+    public void OnExitButtonClick()
+    {
+        Application.Quit();
+    }
+
+    void OnLobbyCreated(LobbyCreated_t result)
+    {
+        if (result.m_eResult == EResult.k_EResultOK)
+        {
+            Debug.Log("Lobby created -- SUCCESS!");
+            lobby_canvas.enabled = true;
+            main_menu_canvas.enabled = false;
+        }
+        else
+        {
+            Debug.Log("Lobby created -- failure ...");
+        }
+
+        string personalName = SteamFriends.GetPersonaName();
+        SteamMatchmaking.SetLobbyData((CSteamID)result.m_ulSteamIDLobby, "name", personalName+"'s game");
+    }
+
+    void OnGetLobbiesList(LobbyMatchList_t result)
+    {
+        Debug.Log("Found " + result.m_nLobbiesMatching + " lobbies!");
+        for(int i=0; i< result.m_nLobbiesMatching; i++)
+        {
+            CSteamID lobbyID = SteamMatchmaking.GetLobbyByIndex(i);
+            lobbyIDS.Add(lobbyID);
+            SteamMatchmaking.RequestLobbyData(lobbyID);
+        }
+    }
+
+    void OnGetLobbyInfo(LobbyDataUpdate_t result)
+    {
+        for(int i=0; i<lobbyIDS.Count; i++)
+        {
+            if (lobbyIDS[i].m_SteamID == result.m_ulSteamIDLobby)
+            {
+                Debug.Log("Lobby " + i+" :: " +SteamMatchmaking.GetLobbyData((CSteamID)lobbyIDS[i].m_SteamID, "name"));
+                return;
+            }
+        }
+       
+    }
+
+    void OnLobbyEntered(LobbyEnter_t result)
+    {
+        current_lobbyID = result.m_ulSteamIDLobby;
+
+        if (result.m_EChatRoomEnterResponse == 1)
+        {
+            main_menu_canvas.enabled = false;
+            lobby_canvas.enabled = true;
+            Debug.Log("Lobby joined!");
+            lobbyID = (Steamworks.CSteamID) current_lobbyID;
+            players_in_lobby = SteamMatchmaking.GetNumLobbyMembers(lobbyID);
+            update_lobby_labels();
+        }   
+        else
+        {
+            Debug.Log("Failed to join lobby.");
+        }
+    }
+
+    void OnJoinRequested(GameLobbyJoinRequested_t result)
+    {
+        SteamMatchmaking.JoinLobby(result.m_steamIDLobby);
+    }
+
+    void OnChatUpdate(LobbyChatUpdate_t result)
+    {
+        update_lobby_labels();
+    }
+
+    void update_lobby_labels()
+    {
+        for (int i = 0; i < players_in_lobby; i++)
+        {
+            CSteamID tmpID = SteamMatchmaking.GetLobbyMemberByIndex(lobbyID, i);
+            player_labels[i].text = SteamFriends.GetFriendPersonaName(tmpID);
+        }   
     }
 
     // Update is called once per frame
