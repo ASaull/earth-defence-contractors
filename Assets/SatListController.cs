be97744f2887361cf24af540ef6d0788e5cc8c2f
BuildTools <unconfigured@null.spigotmc.org>
2020-01-08 18:41:38 -0500

    satellites can now be previewed

--- Assets/SatListController.cs
+++ Assets/SatListController.cs
@@ -1,21 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEditor;
 
-[ExecuteInEditMode]
 public class SatListController : MonoBehaviour
 {
     string[] satellites;
+    public GameObject base_button;
+
+    private GameObject current_sat = null;
 
     // Start is called before the first frame update
-    void Awake()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
         // we will search the Assets/Satellites directory to find
         // the available satellite types to add them to the list
@@ -24,8 +21,30 @@ public class SatListController : MonoBehaviour
         {
             string path = AssetDatabase.GUIDToAssetPath(satellite).Substring(17);
             GameObject tmpSat = Resources.Load<GameObject>(path.Remove(path.Length - 7));
-            Debug.Log(path.Remove(path.Length - 7));
-            Debug.Log(tmpSat.GetComponent<SatelliteController>().sat_name);
+            //we now have to create a button to allow us to create a tmpsat
+            GameObject tmp_button = (GameObject)Instantiate(base_button, transform);
+            tmp_button.GetComponentInChildren<Text>().text = tmpSat.GetComponent<SatelliteController>().sat_name;
+            tmp_button.GetComponent<Button>().onClick.AddListener(delegate{Preview(tmpSat);});
         }
+
+
+    }
+
+    void Preview(GameObject sat)
+    {
+        if (current_sat == null)
+        {
+            current_sat = Instantiate(sat, new Vector3(0, 0, 0), Quaternion.identity);
+        }
+        else
+        {
+            Destroy(current_sat);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
     }
 }
